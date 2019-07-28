1022b61688f4a19e5c6f604b4c86e14de7b6b08a
Lauren Welsh <laurenwelsh@laurems-mbp.home>
2019-07-28 09:43:39 +0100

    initial server commit

---
+++ server/helpers/googleSheetsHelper.js
@@ -0,0 +1,64 @@
+const { google } = require('googleapis');
+const privatekey = require("./WeddingApp-9068afdac0b5.json");
+const sheets = google.sheets('v4');
+
+const createAndConnectJwtClient = async () => {
+  const jwtClient = new google.auth.JWT(
+    privatekey.client_email,
+    null,
+    privatekey.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+  );
+
+  try {
+    await jwtClient.authorize();
+  }
+  catch (error) {
+    console.error('Failed authenticating with Google', error);
+    process.exit(1);
+  }
+
+  return jwtClient;
+}
+
+const retrieveAndPrintSheet = async (jwtClient, sheetId) => {
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      auth: jwtClient,
+      spreadsheetId: sheetId,
+      range: 'A1:D4'
+    });
+
+    for (let row of response.data.values) {
+      console.log('Title [%s]\t\tRating [%s]', row[0], row[1]);
+    }
+  }
+  catch (error) {
+    console.error('Failed retrieving sheet data', error);
+    process.exit(1);
+  }
+}
+
+const updateAndPrintSheet = async (jwtClient, spreadsheetId, values) => {
+  try {
+    await sheets.spreadsheets.values.append({
+      auth: jwtClient,
+      spreadsheetId: spreadsheetId,
+      range: 'test!A2:D2',
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values
+      }
+    });
+  }
+  catch (error) {
+    console.error('Failed retrieving sheet data', error);
+    throw error;
+  }
+}
+
+module.exports = {
+  createAndConnectJwtClient,
+  retrieveAndPrintSheet,
+  updateAndPrintSheet
+}
---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const googleSheetsHelper = require('./helpers/googleSheetsHelper');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'lauren.mae.welsh@gmail.com',
+        pass: 'M@nygat3s'
+    }
+});
+
+app.use(cors());
+app.use(require("body-parser").json());
+
+app.listen(3001, function() {
+    console.log('listening on 3001');
+});
+
+app.post('/test/', async (req, res) => {
+
+    try {
+        const spreadsheetValues = [Object.values(req.body)];
+        await updateSpreadsheet(spreadsheetValues);
+        sendEmail(`RSVP, ${req.body.name}`, JSON.stringify(req.body));
+        res.sendStatus(200);
+    } catch (err) {
+        throw err;
+    }
+});
+
+const updateSpreadsheet = async(spreadsheetValues) => {
+    try {
+        const jwtClient = await googleSheetsHelper.createAndConnectJwtClient();
+        const weddingSpreadsheetId = '1hA-6gL8cQfmUtkGAKGuZYJBNkupgb0aB_3tDciPM15I';
+        await googleSheetsHelper.updateAndPrintSheet(jwtClient, weddingSpreadsheetId, spreadsheetValues);
+    } catch (err) {
+        console.error(`Error updating spreadsheet: ${err}`);
+        sendEmail('Wedding App Error', `Updating spreadsheet error: ${JSON.stringify(err)}`);
+        throw error;
+    }
+};
+
+const sendEmail = async (subject, text) => {
+    try {
+        let info = await transporter.sendMail({
+            from: '"Wedding App" <lauren.mae.welsh@gmail.com>',
+            to: "lauren.mae.welsh@gmail.com",
+            subject,
+            text
+        });
+    
+        console.info(`Message sent: ${info.messageId}`);
+    } catch (e) {
+        console.error(`Error sending email: ${err}`);
+        throw error;
+    }
+};
