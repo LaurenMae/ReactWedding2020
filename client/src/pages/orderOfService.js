89a9eb8981d487ee1ff9481e0d804ac506eca7bf
Lauren Welsh <wela9001@GBLC02YM15TLVCH.local>
2019-07-29 16:21:45 +0100

    making oos mobile friendly WIP

--- client/src/pages/orderOfService.js
+++ client/src/pages/orderOfService.js
@@ -5,31 +5,21 @@ export default function OrderOfService() {
     return (
         <div>
             <Navbar />
-            <div className="container">
-                <p>
-                    <b>12.45 - 13.15</b><br />
-                    Arrival Drinks<br />
-                    <b>13.15 - 13.30<br /></b>
-                    Please take your seats<br />
-                    <b>13.30 - 14.00<br /></b>
-                    Ceremony<br />
-                    <b>14.00 - 16.00<br /></b>
-                    Fizz & Photos<br />
-                    <b>16.00 - 18.30<br /></b>
-                    Speeches & Dinner<br />
-                    <b>18.40<br /></b>
-                    Cake Cutting<br />
-                    <b>19.00<br /></b>
-                    Evening Guests Arrival<br />
-                    <b>19.30<br /></b>
-                    First Dance<br />
-                    <b>20.30<br /></b>
-                    Dinner is Served<br />
-                    <b>20.30 - 01.00<br /></b>
-                    Dancing<br />
-                    <b>01.00<br /></b>
-                    Hometime<br />
-                </p>
+            <div>
+                <h3>1:15pm</h3>
+                Please take your seats
+                <h3>1:30pm</h3>
+                Ceremony
+                <h3>2:00pm</h3>
+                Fizz & Photos
+                <h3>4:00pm</h3>
+                Speeches & Dinner
+                <h3>7.30pm</h3>
+                First Dance
+                <h3>8:30pm</h3>
+                Dinner is Served
+                <h3>1:00am</h3>
+                Hometime
             </div>
         </div>
     );
