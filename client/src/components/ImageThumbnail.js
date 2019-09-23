113f27527171c9582beb76b8e4e7224ab8e4d3a7
Lauren Welsh <laurenwelsh@laurems-mbp.home>
2019-09-23 23:17:47 +0100

    update container error

--- client/src/components/ImageThumbnail.js
+++ client/src/components/ImageThumbnail.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Container } from 'reactstrap';
 import { Image } from 'react-bootstrap';
 
 import './imageThumbnail.scss';
 
 export default function ImageThumbnail({ image, children }) {
     return (
-        <Container className='image-thumbnail-container'>
+        <div className='image-thumbnail-container'>
             <div className='image-container'>
                 <Image src={require(`../images/${image}`)} rounded />
             </div>
             <div className='text-container'>
                 {children}
             </div>
-        </Container>
+        </div>
     )
 }
